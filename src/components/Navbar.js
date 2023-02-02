4bddfd238b7c6194f9cabfca61b9cdb418236f32
LukeMocroski <devmocroski@gmail.com>
2023-02-02 11:10:13 -0300

    Creating Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-gray-700'>
+    <div className='h-16 px-7 flex items-center'>
+      <p className='text-white font-semibold'>Employee Management</p>
+    </div>
+  </div>
+  )
+}
+
+export default Navbar
