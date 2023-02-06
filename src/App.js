f591dac7f546a2810a6081fc4c9837c4d7e3b50d
LukeMocroski <devmocroski@gmail.com>
2023-02-06 11:36:14 -0300

    Add employeList component and routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import AddEmployee from './components/AddEmployee';
+import EmployeeList from './components/EmployeeList';
 
 import Navbar from './components/Navbar';
 
 function App() {
  return (
   <>
+  <BrowserRouter>
     <Navbar />
-    <AddEmployee/>
+    <Routes>
+      <Route index element={<EmployeeList/>}/>
+      <Route path="/" element={<EmployeeList/>}></Route>
+      <Route path='/employeeList' element={<EmployeeList/>}></Route>
+      <Route path='/addEmployee' element={<AddEmployee/>}></Route>
+    </Routes>
+  </BrowserRouter>
   </>
  );
 }
